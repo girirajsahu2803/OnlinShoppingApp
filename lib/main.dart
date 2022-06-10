515db2c6ca1a8886f539e88350ab655df31e4684
girirajSahu <https://github.com/girirajsahu2803/KimberChatApp>
2022-06-10 11:59:57 +0530

    day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shopapp/pages/homepage.dart';
 import 'package:shopapp/pages/secondScreen.dart';
 import 'package:shopapp/pages/loginpage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,8 +14,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
-      theme: ThemeData(primarySwatch: Colors.purple),
+      initialRoute: '/logIn',
+      theme: ThemeData(
+        fontFamily: GoogleFonts.lato().fontFamily,
+        primarySwatch: Colors.purple,
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
       routes: {
         '/': (context) => HomePage(),
         '/2': (context) => SecondScreen(),
--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,13 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('LOGIN PAGE'),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: Text(
+              'LOGIN PAGE',
+              style: GoogleFonts.lato(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                color: Colors.blueAccent,
+              ),
+            ),
+          ),
+          Container(
+            height: 300,
+            width: 300,
+            padding: const EdgeInsets.all(15.0),
+            child: SvgPicture.asset(
+              'images/login.svg',
+              fit: BoxFit.contain,
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    // labelText: 'UserName',
+                    hintText: 'Enter UserName',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    // labelText: 'UserPassword',
+                    hintText: 'Enter Password',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                    // foregroundColor: MaterialStateProperty<Color>(),
+                    ),
+                onPressed: () {},
+                child: Text(
+                  ' LOGIN ',
+                  style: GoogleFonts.lato(color: Colors.white),
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
